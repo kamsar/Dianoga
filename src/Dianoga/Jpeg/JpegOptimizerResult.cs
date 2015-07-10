d3fcbde62a860a948d66f09e4c67707dda33490f
Kamruz Jaman <jammykam@gmail.com>
2015-07-10 18:22:58 +0100

    Added size check to ensure new file size smaller. Fixed null object in exception Added bit check in nQuantOptimizer

--- src/Dianoga/Jpeg/JpegOptimizerResult.cs
+++ src/Dianoga/Jpeg/JpegOptimizerResult.cs
@@ -4,11 +4,11 @@ namespace Dianoga.Jpeg
 {
 	public class JpegOptimizerResult : IOptimizerResult
 	{
-		public bool Success { get; internal set; }
-		public string ErrorMessage { get; internal set; }
-		public int SizeBefore { get; internal set; }
-		public int SizeAfter { get; internal set; }
-		public Stream ResultStream { get; internal set; }
+		public bool Success { get; set; }
+		public string ErrorMessage { get; set; }
+		public int SizeBefore { get; set; }
+		public int SizeAfter { get; set; }
+		public Stream ResultStream { get; set; }
 
 		public Stream CreateResultStream()
 		{
