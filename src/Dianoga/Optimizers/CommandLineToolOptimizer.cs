6a43749d5b61af3099c37371b3d24eb43b3f21a2
Kam Figy <kamsar@gmail.com>
2016-03-11 13:16:49 -0800

    Moved tools under app_data to make them unserveable

--- src/Dianoga/Optimizers/CommandLineToolOptimizer.cs
+++ src/Dianoga/Optimizers/CommandLineToolOptimizer.cs
@@ -23,6 +23,11 @@ namespace Dianoga.Optimizers
 			}
 		}
 
+		/// <summary>
+		/// Shell execute uses PATH and can run non-exe files (e.g. cmd) but it cannot capture output
+		/// </summary>
+		protected bool UseShellExecute { get; set; }
+
 		protected override void ProcessOptimizer(OptimizerArgs args)
 		{
 			var tempFilePath = GetTempFilePath();
@@ -38,33 +43,8 @@ namespace Dianoga.Optimizers
 					args.Stream.Dispose();
 				}
 
-				var processOutput = new List<string>();
-
-				var processInfo = new ProcessStartInfo();
-				processInfo.UseShellExecute = false;
-				processInfo.RedirectStandardOutput = true;
-				processInfo.RedirectStandardError = true;
-				processInfo.FileName = ExePath;
-				processInfo.Arguments = arguments;
-
-				var toolProcess = new Process();
-				toolProcess.StartInfo = processInfo;
-				toolProcess.OutputDataReceived += (sender, eventArgs) => processOutput.Add(eventArgs.Data);
-				toolProcess.ErrorDataReceived += (sender, eventArgs) => processOutput.Add(eventArgs.Data);
-
-				toolProcess.Start();
-				toolProcess.BeginOutputReadLine();
-				toolProcess.BeginErrorReadLine();
-
-				if (!toolProcess.WaitForExit(ToolTimeout))
-				{
-					throw new InvalidOperationException($"{ExePath} took longer than {ToolTimeout}ms to run, which is a failure. Output: {string.Join(Environment.NewLine, processOutput)}");
-				}
-
-				if (toolProcess.ExitCode != 0)
-				{
-					throw new InvalidOperationException($"{ExePath} exited with unexpected exit code {toolProcess.ExitCode}. Output: {string.Join(Environment.NewLine, processOutput)}");
-				}
+				if(UseShellExecute) ExecuteShell(arguments);
+				else ExecuteProcess(arguments);
 
 				// read the file to memory so we can nuke the temp file
 				using (var fileStream = File.OpenRead(tempOutputPath))
@@ -80,6 +60,57 @@ namespace Dianoga.Optimizers
 			}
 
 			args.IsOptimized = true;
+		}
+
+		protected virtual void ExecuteProcess(string arguments)
+		{
+			var processOutput = new List<string>();
+
+			var processInfo = new ProcessStartInfo();
+			processInfo.UseShellExecute = false;
+			processInfo.RedirectStandardOutput = true;
+			processInfo.RedirectStandardError = true;
+			processInfo.FileName = ExePath;
+			processInfo.Arguments = arguments;
+
+			var toolProcess = new Process();
+			toolProcess.StartInfo = processInfo;
+			toolProcess.OutputDataReceived += (sender, eventArgs) => processOutput.Add(eventArgs.Data);
+			toolProcess.ErrorDataReceived += (sender, eventArgs) => processOutput.Add(eventArgs.Data);
+
+			toolProcess.Start();
+			toolProcess.BeginOutputReadLine();
+			toolProcess.BeginErrorReadLine();
+
+			if (!toolProcess.WaitForExit(ToolTimeout))
+			{
+				throw new InvalidOperationException($"{ExePath} took longer than {ToolTimeout}ms to run, which is a failure. Output: {string.Join(Environment.NewLine, processOutput)}");
+			}
+
+			if (toolProcess.ExitCode != 0)
+			{
+				throw new InvalidOperationException($"{ExePath} exited with unexpected exit code {toolProcess.ExitCode}. Output: {string.Join(Environment.NewLine, processOutput)}");
+			}
+		}
+
+		protected virtual void ExecuteShell(string arguments)
+		{
+			var processInfo = new ProcessStartInfo();
+			processInfo.UseShellExecute = true;
+			processInfo.FileName = ExePath;
+			processInfo.Arguments = arguments;
+
+			var toolProcess = System.Diagnostics.Process.Start(processInfo);
+
+			if (!toolProcess.WaitForExit(ToolTimeout))
+			{
+				throw new InvalidOperationException($"{ExePath} took longer than {ToolTimeout}ms to run, which is a failure. Output not available using shell execute.");
+			}
+
+			if (toolProcess.ExitCode != 0)
+			{
+				throw new InvalidOperationException($"{ExePath} exited with unexpected exit code {toolProcess.ExitCode}. Output not available using shell execute.");
+			}
 		}
 
 		protected abstract string CreateToolArguments(string tempFilePath, string tempOutputPath);
