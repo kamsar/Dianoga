6e5e9042e37ff8769184f2f72f366a6308ac8496
Kam Figy <kamsar@gmail.com>
2016-03-11 13:17:18 -0800

    Add SVGO SVG optimizer

---
+++ src/Dianoga/Optimizers/Pipelines/DianogaSvg/SvgoOptimizer.cs
@@ -0,0 +1,16 @@
+﻿namespace Dianoga.Optimizers.Pipelines.DianogaSvg
+{
+	/// <summary>
+	/// Uses SVGO to optimize SVGs at runtime.
+	/// Must install SVGO (and Node.js) globally in path for this to work.
+	/// </summary>
+	public class SvgoOptimizer : CommandLineToolOptimizer
+	{
+		protected override string CreateToolArguments(string tempFilePath, string tempOutputPath)
+		{
+			UseShellExecute = true;
+
+			return $"--input=\"{tempFilePath}\" --output=\"{tempOutputPath}\"";
+		}
+	}
+}
