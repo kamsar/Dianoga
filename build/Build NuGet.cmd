b1c17fc62ef189a5795e41c26acaed11c5afd058
Kam Figy <kamsar@gmail.com>
2016-03-11 22:06:58 -0800

    Prevent NuGet from ignoring some SVGO important files :)

--- build/Build NuGet.cmd
+++ build/Build NuGet.cmd
@@ -1,5 +1,5 @@
 
 @echo off
 "%~dp0..\tools\nuget.exe" restore "%~dp0..\Dianoga.sln"
-"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga.csproj" -Build -Symbols -Properties Configuration=Release
+"%~dp0..\tools\nuget.exe" pack "%~dp0..\src\Dianoga\Dianoga.csproj" -Build -Symbols -Properties Configuration=Release -NoDefaultExcludes
 PAUSE
