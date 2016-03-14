445711e04bcaa655184c1a524aa69220129a9e41
Kam Figy <kamsar@gmail.com>
2016-03-13 20:22:22 -0700

    Enable SVGs to be optimized correctly when using the synchronous optimization strategy

--- src/Dianoga/Svg/Pipelines/GetMediaStream/SvgIgnorer.cs
+++ src/Dianoga/Svg/Pipelines/GetMediaStream/SvgIgnorer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dianoga.Invokers.GetMediaStreamSync;
+using Dianoga.Invokers.MediaCacheAsync;
 using Sitecore.Diagnostics;
 using Sitecore.Resources.Media;
 
@@ -10,11 +12,21 @@ namespace Dianoga.Svg.Pipelines.GetMediaStream
 	/// </summary>
 	public class SvgIgnorer
 	{
+		public bool SynchronouslyOptimizeSvgs { get; set; }
+
 		public void Process(GetMediaStreamPipelineArgs args)
 		{
 			Assert.ArgumentNotNull(args, "args");
+
 			if (args.MediaData.MimeType.Equals("image/svg+xml", StringComparison.Ordinal))
+			{
+				// if the synchronous flag is set, the SVG will be optimized before we abort the pipeline to prevent
+				// Sitecore from trying to process an SVG. Set this flag when NOT using the media cache async optimization strategy.
+				if(SynchronouslyOptimizeSvgs && !(MediaManager.Cache is OptimizingMediaCache))
+					new OptimizeImage().Process(args);
+
 				args.AbortPipeline();
+			}
 		}
 	}
 }
