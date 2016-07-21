585c08400292de18247eb4d54f2cc7f4c6e73f13
Kam Figy <kamsar@gmail.com>
2016-07-21 15:10:21 -0700

    3,0b3

--- src/Dianoga/Properties/AssemblyInfo.cs
+++ src/Dianoga/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta02")]
+[assembly: AssemblyInformationalVersion("3.0.0-beta03")]
