49bc6b8adc054d15e0cfc40a33ecde7e91a8bc54
Kam Figy <kamsar@gmail.com>
2016-07-21 20:26:24 -0700

    Ya know what this is an RC1 :D

--- src/Dianoga/Properties/AssemblyInfo.cs
+++ src/Dianoga/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta04")]
+[assembly: AssemblyInformationalVersion("3.0.0-RC01")]
