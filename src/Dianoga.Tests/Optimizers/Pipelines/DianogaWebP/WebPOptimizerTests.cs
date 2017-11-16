9addef15b6a38aca515ed816cc616146c085854a
Anton Tishchenko <antontishchenko@gmail.com>
2017-11-16 19:10:04 +0200

    WebP implementation: initial commit

---
+++ src/Dianoga.Tests/Optimizers/Pipelines/DianogaWebP/WebPOptimizerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Dianoga.Optimizers;
+using Dianoga.Optimizers.Pipelines.DianogaWebP;
+using FluentAssertions;
+using Xunit;
+
+namespace Dianoga.Tests.Optimizers.Pipelines.DianogaWebP
+{
+	public class WebPOptimizerTests
+    {
+		[Fact]
+		public void ShouldSquishTestJpeg()
+		{
+			var inputStream = new MemoryStream();
+
+			using (var testJpeg = File.OpenRead(@"Optimizers\Pipelines\DianogaWebP\test.jpg"))
+			{
+				testJpeg.CopyTo(inputStream);
+			}
+
+			var sut = new WebPOptimizer();
+			sut.ExePath = @"..\..\..\Dianoga\Dianoga Tools\libwebp-0.4.1-windows-x64\bin\cwebp.exe";
+
+			var args = new OptimizerArgs(inputStream);
+
+			var startingSize = args.Stream.Length;
+
+			sut.Process(args);
+
+			args.Stream.Length.Should().BeLessThan(startingSize).And.BeGreaterThan(0);
+			args.IsOptimized.Should().BeTrue();
+		}
+	}
+}
---
+++ src/Dianoga/Optimizers/Pipelines/DianogaWebP/WebPOptimizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dianoga.Optimizers.Pipelines.DianogaWebP
+{
+    public class WebPOptimizer : CommandLineToolOptimizer
+    {
+        protected override string CreateToolArguments(string tempFilePath, string tempOutputPath)
+        {
+            return $"\"{tempFilePath}\" -o \"{tempOutputPath}\" ";
+        }
+    }
+}
