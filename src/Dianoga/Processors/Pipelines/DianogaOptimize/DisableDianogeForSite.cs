33d5bc008ef5232448c65986a4d27dc3a45d89de
Bartłomiej Mucha <bartlomiej.mucha@hotmail.com>
2018-11-19 15:18:29 +0100

    DisableDianogaForSite processor

---
+++ src/Dianoga/Processors/Pipelines/DianogaOptimize/DisableDianogeForSite.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore;
+
+namespace Dianoga.Processors.Pipelines.DianogaOptimize
+{
+	public class DisableDianogeForSite
+	{
+		public virtual void Process(ProcessorArgs args)
+		{
+			if (Context.Site.Properties["enableDianoga"] == "false")
+			{
+				args.AbortPipeline();
+			}
+		}
+	}
+}
