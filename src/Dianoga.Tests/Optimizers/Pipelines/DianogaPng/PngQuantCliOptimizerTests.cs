22d0d36a3e212b5af7fa0fc063e2a96ee5804dfb
Mark Gibbons <markgibbons25@gmail.com>
2020-02-28 11:36:19 +1000

    Add PngQuantCliOptimizerTests

---
+++ src/Dianoga.Tests/Optimizers/Pipelines/DianogaPng/PngQuantCliOptimizerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Dianoga.Optimizers;
+using Dianoga.Optimizers.Pipelines.DianogaPng;
+using FluentAssertions;
+using Xunit;
+
+namespace Dianoga.Tests.Optimizers.Pipelines.DianogaPng
+{
+	public class PngQuantCliOptimizerTests
+	{
+		[Fact]
+		public void ShouldSquishTestPng()
+		{
+			var inputStream = new MemoryStream();
+
+			using (var testPng = File.OpenRead(@"Optimizers\Pipelines\DianogaPng\test.png"))
+			{
+				testPng.CopyTo(inputStream);
+			}
+
+			var sut = new PngQuantCliOptimizer();
+			sut.ExePath = @"..\..\..\Dianoga\Dianoga Tools\pngquant\pngquant.exe";
+
+			var args = new OptimizerArgs(inputStream);
+
+			var startingSize = args.Stream.Length;
+
+			sut.Process(args);
+
+			args.Stream.Length.Should().BeLessThan(startingSize).And.BeGreaterThan(0);
+			args.IsOptimized.Should().BeTrue();
+		}
+	}
+}
