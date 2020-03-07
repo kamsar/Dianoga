76b863143c52553dfe83c98097ddf7aa067b563c
Mark Gibbons <markgibbons25@gmail.com>
2020-03-07 12:57:45 +1000

    Fix temp file issue for png #52

--- src/Dianoga/Optimizers/Pipelines/DianogaPng/PngQuantCliOptimizer.cs
+++ src/Dianoga/Optimizers/Pipelines/DianogaPng/PngQuantCliOptimizer.cs
@@ -1,21 +1,13 @@
-﻿using System.IO;
-
-namespace Dianoga.Optimizers.Pipelines.DianogaPng
+﻿namespace Dianoga.Optimizers.Pipelines.DianogaPng
 {
 	// uses pngquant: https://pngquant.org/
-	public class PngQuantCliOptimizer : CommandLineToolOptimizer
+	public class PngQuantCliOptimizer : PngOptimizer
 	{
 		protected override bool OptimizerUsesSeparateOutputFile => false;
 
 		protected override string CreateToolArguments(string tempFilePath, string tempOutputPath)
 		{
 			return $"--force --ext .png \"{tempFilePath}\"";
-		}
-
-		protected override string GetTempFilePath()
-		{
-			// must have a PNG extension and the default gives us .tmp
-			return Path.ChangeExtension(base.GetTempFilePath(), ".png");
 		}
 	}
 }
