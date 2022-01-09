c440896981a03e228d8340509a403636ef71614d
Antonytm <antontishchenko@gmail.com>
2022-01-09 10:14:48 +0200

    JxlOptimizer implementation and tests

---
+++ src/Dianoga/Optimizers/Pipelines/DianogaJxl/JxlOptimizer.cs
@@ -0,0 +1,32 @@
+﻿using Dianoga.NextGenFormats;
+
+namespace Dianoga.Optimizers.Pipelines.DianogaJxl
+{
+	public class JxlOptimizer : CommandLineToolOptimizer
+	{
+		public readonly string Extension = "jxl";
+		public bool DisableResizing { get; set; }
+
+		public override void Process(OptimizerArgs args)
+		{
+			if (args.MediaOptions.CheckSupportOfExtension(Extension))
+			{
+				base.Process(args);
+
+				if (args.IsOptimized)
+				{
+					args.Extension = Extension;
+
+					//If Jpeg-XL optimization was executed then abort running other optimizers
+					//because they don't accept jxl input file format
+					args.AbortPipeline();
+				}
+			}
+		}
+
+		protected override string CreateToolArguments(string tempFilePath, string tempOutputPath)
+		{
+			return $"\"{tempFilePath}\" \"{tempOutputPath}\" ";
+		}
+	}
+}
