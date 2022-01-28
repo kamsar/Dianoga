158ac378df9d97d9ee025f044adb1dd745ec7609
Antonytm <antontishchenko@gmail.com>
2022-01-28 15:17:09 +0200

    Replace svgop.exe with svgo-win.exe

---
+++ src/Dianoga/Dianoga Tools/SVGO/svgo.sample.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          //customize default plugin options
+          //inlineStyles: {
+          //  onlyMatchedOnce: false,
+          //},
+
+          //or disable plugins
+          removeViewBox: false,
+        },
+      },
+    },
+  ],
+};
