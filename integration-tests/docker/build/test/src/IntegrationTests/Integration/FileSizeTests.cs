033a5303894cab7eacdcb020ffe50f92c7fcd15d
Antonytm <antontishchenko@gmail.com>
2022-02-14 15:23:04 +0200

    Integration tests: make sure that they will pass even on slow computers

--- integration-tests/docker/build/test/src/IntegrationTests/Integration/FileSizeTests.cs
+++ integration-tests/docker/build/test/src/IntegrationTests/Integration/FileSizeTests.cs
@@ -74,7 +74,7 @@ namespace Integration
 				initialSize.Should().Be(size, "Original size doesn't match");
 
 				//How to find proper value, how much time file conversion will take?
-				sleepService.Sleep(new TimeSpan(0, 0, 5));
+				sleepService.Sleep(new TimeSpan(0, 0, 20));
 
 				request = WebRequest.Create(CDHostname + url + seed);
 				request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
